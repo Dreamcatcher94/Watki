b6ec6843c144a20321768ae5ec041d1a56b1c3d3
Dreamcatcher94 <b1965501@trbvn.com>
2016-03-15 05:09:38 +0100

    watki
    watki

---
+++ src/watki/Main.java
@@ -0,0 +1,35 @@
+package watki;
+
+import watki.Silnia.Silnia;
+import watki.ZipBomba.ZipBomb;
+
+import java.util.Scanner;
+
+/**
+ * Created by Marta Pawlik on 17.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Wybierz program: \n1 - Obliczanie silni" +
+                " \n2 - Zip bomba");
+
+        System.out.print("Wybór: ");
+        Scanner scanner = new Scanner(System.in);
+
+        switch (scanner.nextInt()) {
+
+            case 1:
+                new Silnia();
+                break;
+
+            case 2:
+                new ZipBomb();
+                break;
+
+        }
+    }
+
+
+}
